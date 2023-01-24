bb5a6a11e3988267b513c82b4bc213b90cfbd6e6
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2023-01-24 20:52:14 +0100

    staticfiles

--- ogb_website/settings.py
+++ ogb_website/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
@@ -150,3 +151,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Configure Django App for Heroku.
+
