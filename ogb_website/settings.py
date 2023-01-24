da1f98a800f8af9d832d483273fb8b19b69cb188
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2023-01-24 15:38:42 +0100

    added database conn

--- ogb_website/settings.py
+++ ogb_website/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'ogb_website.wsgi.application'
     #}
 #}
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -98,6 +99,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
