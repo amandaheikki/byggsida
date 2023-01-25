d21d8b07ec211c6e8917b15f61b74aa92234bd0c
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2023-01-25 16:15:58 +0100

    removed

--- staticfiles/website/script.37adf922d0e5.js
+++
@@ -1,2 +0,0 @@
-function toggleClass(){let menu=document.querySelector(".nav-list");menu.classList.toggle("toggleCls")}
-let hamburger=document.querySelector(".ham-Icon");hamburger.addEventListener("click",toggleClass)
