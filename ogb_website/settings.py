fb0abe1e3ec7ad7de2f1d60bb9bdb06e77ec73d2
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2023-01-25 13:53:34 +0100

    r

--- ogb_website/settings.py
+++ ogb_website/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'website/static'),]
 MEDIA_ROOT = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
