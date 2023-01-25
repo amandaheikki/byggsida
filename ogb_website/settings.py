7213babccf6c1f7df66b95d77f5764ed2a559e9f
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2023-01-25 14:01:06 +0100

    new

--- ogb_website/settings.py
+++ ogb_website/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d5-67kkqh0*w7&zzwe*2w%f&h%z=v5t1-dee4$92yy9qjyl#-j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '*']
 
