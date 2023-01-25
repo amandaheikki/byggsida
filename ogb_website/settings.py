01390e46d86d9ec097665bea3fe5a810f196f2b3
amandaheikki <73876227+amandaheikki@users.noreply.github.com>
2023-01-25 16:20:11 +0100

    commit

--- ogb_website/settings.py
+++ ogb_website/settings.py
@@ -156,10 +156,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
